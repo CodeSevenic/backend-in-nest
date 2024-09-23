2095d06839dfb3bb34ee23a95c72b22abe18cc54
CodeSevenic <sevenicbookings@gmail.com>
2024-09-23 13:32:53 +0200

    NestJS backend code update

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,7 +1,10 @@
-import { MinLength } from 'class-validator';
+import { IsEnum, MinLength } from 'class-validator';
 
 export class CreateNinjaDto {
   @MinLength(3)
   name: string;
+  @IsEnum(['stars', 'nunchucks'], {
+    message: 'Weapon must be either stars or nunchucks',
+  })
   weapon: 'stars' | 'nunchucks';
 }
